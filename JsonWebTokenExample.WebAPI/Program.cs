30c9c7ec982050192f855ca4993c4ef67797e883
hugh19se <hugh19s@icloud.com>
2023-12-13 00:35:57 +0000

    Set Up JWT in program.cs

--- JsonWebTokenExample.WebAPI/Program.cs
+++ JsonWebTokenExample.WebAPI/Program.cs
@@ -1,4 +1,6 @@
 using JsonWebTokenExample.Configs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,29 @@ builder.Services.AddSwaggerGen();
 //Configs
 builder.Services.Configure<JWTConfig>(builder.Configuration.GetSection(JWTConfig.ConfigBinding));
 
+//Add JWT
+JWTConfig _jwtConfig = builder.Configuration.GetSection(JWTConfig.ConfigBinding).Get<JWTConfig>();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}
+).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidIssuer = _jwtConfig.Issuer,
+        ValidAudience = _jwtConfig.Audience,
+        IssuerSigningKey = _jwtConfig.IssuerSigningKey,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +44,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
