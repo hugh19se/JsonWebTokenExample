835a6bf24a9a59400da60d38ad409172f00e777d
hugh19se <hugh19s@icloud.com>
2023-12-13 00:24:24 +0000

    Added JWT AppSettings config

--- JsonWebTokenExample.WebAPI/Program.cs
+++ JsonWebTokenExample.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using JsonWebTokenExample.Configs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configs
+builder.Services.Configure<JWTConfig>(builder.Configuration.GetSection(JWTConfig.ConfigBinding));
 
 var app = builder.Build();
 
